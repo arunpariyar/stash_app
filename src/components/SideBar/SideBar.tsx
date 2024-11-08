2928d2f384c897b952f7a0a3d9cd299fdf72ee46
Arun Pariyar <arunpariyar@MacBookPro.fritz.box>
2024-11-08 12:09:57 +0100

    🚧 Fix icon color on hover and text to appear and disappear on responsive screen
    found out that there is in need a way to convert svg color even if its added as img tag using this code .menu-link:hover > img {

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,27 +6,27 @@ export default function SideBar() {
     {
       name: "home",
       icon: "/images/icon-nav-overview.svg",
-      link: "overview",
+      link: "Overview",
     },
     {
       name: "transactions",
       icon: "/images/icon-nav-transactions.svg",
-      link: "transactions",
+      link: "Transactions",
     },
     {
       name: "budgets",
       icon: "/images/icon-nav-budgets.svg",
-      link: "budgets",
+      link: "Budgets",
     },
     {
       name: "pots",
       icon: "/images/icon-nav-pots.svg",
-      link: "pots",
+      link: "Pots",
     },
     {
       name: "bills",
       icon: "/images/icon-nav-recurring-bills.svg",
-      link: "bills",
+      link: "Bills",
     },
   ];
 
@@ -34,9 +34,11 @@ export default function SideBar() {
     <div className="sidebar-container">
       {navItems.map((item) => (
         <Link className="menu-link" to={`${item.link}`}>
-          <img src={item.icon}></img>
+          <img className="menu-icon" src={item.icon}></img>
 
-          <p>{item.name}</p>
+          <p className="menu-link-text">
+            {item.name[0].toUpperCase() + item.name.substring(1)}
+          </p>
         </Link>
       ))}
     </div>
