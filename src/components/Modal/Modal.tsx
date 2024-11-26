bec6d2a69c556165764182ecc285702632f91438
Arun Pariyar <arun.pariyar@gmail.com>
2024-11-26 18:42:49 +0100

    🚧 Fix both open and close model working

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,21 +1,63 @@
 import styles from "./Model.module.css";
-import { PropsWithChildren, ReactNode } from "react";
+import {
+  cloneElement,
+  PropsWithChildren,
+  ReactElement,
+  useContext,
+} from "react";
 import closeMenu from "/images/icon-close-modal.svg";
 import { createPortal } from "react-dom";
-
-interface ModalProps {
-  children: ReactNode;
-  closeModal: () => void;
+import { createContext } from "react";
+import { useState } from "react";
+interface WindowProps {
+  children: ReactElement;
+  name: string;
 }
 
-export default function Modal({ children, closeModal }: ModalProps) {
+interface ModalContextType {
+  openName: string;
+  open: (name: string) => void;
+  close: () => void;
+}
+
+const defaultValue: ModalContextType = {
+  openName: "",
+  open: () => {},
+  close: () => {},
+};
+
+//creating the Model context
+const ModalContext = createContext(defaultValue);
+
+export default function Modal({ children }: PropsWithChildren) {
+  const [openName, setOpenName] = useState("");
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }: WindowProps) {
+  const { openName, close } = useContext(ModalContext);
+
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
       <StyledModal>
-        <button onClick={closeModal} className={styles.closeMenuBtn}>
+        <button className={styles.closeMenuBtn}>
           <img src={closeMenu} alt="" />
         </button>
-        {children}
+        {cloneElement(children, { onCloseModal: close })}
       </StyledModal>
     </Overlay>,
     document.body
@@ -33,3 +75,6 @@ function Overlay({ children }: PropsWithChildren) {
     </div>
   );
 }
+
+Modal.Open = Open;
+Modal.Window = Window;
---
+++ src/pages/Pots/AddPot/Form.tsx
@@ -0,0 +1,13 @@
+export default function AddPotForm({ onCloseModel }: any) {
+  return (
+    <form>
+      <div>
+        <h1>this is the add pot from</h1>
+      </div>
+
+      <div>
+        <button onClick={onCloseModel}>close</button>
+      </div>
+    </form>
+  );
+}
