7f6e3a4fbafb2a653096eb022dafb0357c5e9e6c
Arun Pariyar <arun.pariyar@gmail.com>
2024-12-02 14:31:59 +0100

    ✨ Create BlackBtn and implement to AddPot Modal

--- src/pages/Pots/AddPot/AddPot.tsx
+++ src/pages/Pots/AddPot/AddPot.tsx
@@ -1,14 +1,14 @@
 import Modal from "../../../components/Modal/Modal";
 import AddNewPotForm from "../../../components/Forms/AddNewPotForm/AddNewPotForm";
-
+import BlackBtn from "../../../components/Buttons/BlackBtn/BlackBtn";
 export default function AddPot() {
   return (
     <Modal>
+      {/* {the empty function onClick and OnCloseModal is being passed just as place holder the actual onCloseModal is passed while cloning element} */}
       <Modal.Open opens="pot-form">
-        <button>+ Add Pot</button>
+        <BlackBtn onClick={() => {}} label="+ Add New Pot"></BlackBtn>
       </Modal.Open>
       <Modal.Window name="pot-form">
-        {/* {the empty function is being passed just as place holder the actual onCloseModal is passed while cloning element} */}
         <AddNewPotForm onCloseModal={() => {}}></AddNewPotForm>
       </Modal.Window>
     </Modal>
